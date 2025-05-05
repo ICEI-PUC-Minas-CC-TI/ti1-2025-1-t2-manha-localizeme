0578252d264a04bbcaedcfcbdb4308db4aed4896
Ane Madjarian Viana <madjarianviana@gmail.com>
2025-05-05 18:31:16 -0300

    Informações bares

---
+++ codigo/public/infos.js
@@ -0,0 +1,32 @@
+const dados = {
+    "destinos": [
+    {
+        "id": 1,
+        "descricao": "",
+        "categoria": "",
+        "destaque": true,
+        "Avaliacoes": "",
+        "cardapio": "",
+        "instagram": "",
+        "endereco": "",
+        "Maps": "",
+        "imagens_complementares": [
+            {
+                "id": 1,
+                "src": "",
+                "descricao": ""
+            },
+            {
+                "id": 2,
+                "src": "",
+                "descricao": ""
+            },
+            {
+                "id": 3,
+                "src": "",
+                "descricao": ""
+            }
+        ]
+    }
+    ]
+}
